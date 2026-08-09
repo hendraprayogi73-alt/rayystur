e2f7a94fe65fde0b6b5bfa53586ff3fa66958a77
hendraprayogi73-alt <hendraprayogi73@gmail.com>
2026-08-09 09:11:24 -0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,26 @@
+'pgsql' => [
+
+    'driver' => 'pgsql',
+
+    'url' => env('DB_URL'),
+
+    'host' => env('DB_HOST'),
+
+    'port' => env('DB_PORT'),
+
+    'database' => env('DB_DATABASE'),
+
+    'username' => env('DB_USERNAME'),
+
+    'password' => env('DB_PASSWORD'),
+
+    'charset' => 'utf8',
+
+    'prefix' => '',
+
+    'prefix_indexes' => true,
+
+    'search_path' => 'capgame',
+
+    'sslmode' => 'require',
+],
